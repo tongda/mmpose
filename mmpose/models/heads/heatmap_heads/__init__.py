08010abbdfaf42bc57d256fa6b8e8698ba442c22
674106399@qq.com <674106399@qq.com>
2022-09-28 17:58:39 +0800

    add naive rtm_head based on rle

--- mmpose/models/heads/heatmap_heads/__init__.py
+++ mmpose/models/heads/heatmap_heads/__init__.py
@@ -2,7 +2,10 @@
 from .cpm_head import CPMHead
 from .heatmap_head import HeatmapHead
 from .mspn_head import MSPNHead
+from .rtm_head import RTMHead
 from .simcc_head import SimCCHead
 from .vipnas_head import ViPNASHead
 
-__all__ = ['HeatmapHead', 'CPMHead', 'MSPNHead', 'ViPNASHead', 'SimCCHead']
+__all__ = [
+    'HeatmapHead', 'CPMHead', 'MSPNHead', 'ViPNASHead', 'SimCCHead', 'RTMHead'
+]
