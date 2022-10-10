6c337b28d832f37d2158600f219f40c437a42b45
Tau-J <674106399@qq.com>
2022-10-10 17:28:50 +0800

    prepare sampling

--- mmpose/models/heads/heatmap_heads/__init__.py
+++ mmpose/models/heads/heatmap_heads/__init__.py
@@ -6,9 +6,11 @@ from .simcc_gap_fc_head import SimCC_GAP_FC
 from .simcc_head import SimCCHead
 from .simcc_ipr_head import SimCC_IPR_Head
 from .simcc_rle_head import SimCC_RLE_Head
+from .simcc_sampling_head import SimCC_SamplingArgmax_Head
 from .vipnas_head import ViPNASHead
 
 __all__ = [
     'HeatmapHead', 'CPMHead', 'MSPNHead', 'ViPNASHead', 'SimCCHead',
-    'SimCC_RLE_Head', 'SimCC_GAP_FC', 'SimCC_IPR_Head'
+    'SimCC_RLE_Head', 'SimCC_GAP_FC', 'SimCC_IPR_Head',
+    'SimCC_SamplingArgmax_Head'
 ]
